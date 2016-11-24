cd6457348903b73a0fd43403326d8b72791abb0a
Kimmo Puputti <kimmo@sharetribe.com>
2016-11-24 16:59:29 +0200

    Add ManageAvailabilityHeader component

---
+++ client/app/components/composites/ManageAvailabilityHeader/ManageAvailabilityHeader.js
@@ -0,0 +1,36 @@
+import { PropTypes } from 'react';
+import { div, span } from 'r-dom';
+import { t } from '../../../utils/i18n';
+
+import css from './ManageAvailabilityHeader.css';
+
+const ManageAvailabilityHeader = (props) => div(
+  {
+    className: css.root,
+    style: {
+      height: `${props.height}px`,
+      backgroundImage: `url(${props.imageUrl})`,
+    },
+  },
+  [
+    div({
+      className: css.colorLayer,
+      style: {
+        backgroundColor: `#${props.backgroundColor}`,
+      },
+    }),
+    div({ className: css.listingHeader }, [
+      span({ className: css.availabilityHeader }, t('web.listings.edit_availability_header')),
+      props.title,
+    ]),
+  ]
+);
+
+ManageAvailabilityHeader.propTypes = {
+  backgroundColor: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  height: PropTypes.number.isRequired,
+};
+
+export default ManageAvailabilityHeader;
